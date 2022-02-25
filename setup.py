4d4a428ea321d82125ca7e513383ae95f16b5805
landmaster135 <52403447+landmaster135@users.noreply.github.com>
2022-02-25 15:11:17 +0900

    f

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 setup(
     name="landmasterlibrary",
     version="0.0.1",
-    description="Convenient tools for me.17",
+    description="Convenient tools for me.18",
     author="kinkinnbeer135ml",
     author_email="52403447+landmaster135@users.noreply.github.com",
     license="MIT",
@@ -24,9 +24,9 @@ setup(
     # packages=find_packages(where="src"),
     packages=["src/landmasterlibrary"],
     # package_dir={"": "src"},
-    # package_dir={"": "src/landmasterlibrary"},
+    package_dir={"find_packages": "src/landmasterlibrary"},
     # py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
-    # py_modules=["src"],
+    # py_modules=["landmasterlibrary"],
     include_package_data=True,
     zip_safe=False,
     classifiers=[

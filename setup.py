f4b0ce89d79fce5321aa59509a8e80957785e28e
landmaster135 <52403447+landmaster135@users.noreply.github.com>
2022-02-25 23:00:57 +0900

    update text_replacer

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         "Operating System :: OS Independent",
     ],
     entry_points = {
-        'console_scripts': ['text_editor = landmasterlibrary.text_replacer:main']
+        'text_replacer': ['text_editor = landmasterlibrary.text_replacer:main']
     },
     python_requires=">=3.7",
     install_requires=_requires_from_file("requirements.txt"),
--- test/test_text_replacer.py
+++ test/test_text_replacer.py
@@ -1,58 +1,86 @@
 from src.landmasterlibrary.text_replacer import ReplaceCharacter
 
 class Test_ReplaceCharacter:
-    # Unicode : \u3099
+    # Unicode : \u3099: normal system
     def test_make_voicedsound_1_1(self):
         replaceCharacter = ReplaceCharacter()
-        assert replaceCharacter.make_voicedsound('がぎぐげござじずぜぞ') == 'がぎぐげござじずぜぞ'
+        assert replaceCharacter.make_voicedsound("がぎぐげござじずぜぞ") == "がぎぐげござじずぜぞ"
 
     def test_make_voicedsound_1_2(self):
         replaceCharacter = ReplaceCharacter()
-        assert replaceCharacter.make_voicedsound('だぢづでどばびぶべぼ') == 'だぢづてどばびぶべぼ'
+        assert replaceCharacter.make_voicedsound("だぢづでどばびぶべぼ") == "だぢづでどばびぶべぼ"
 
-    # Unicode : \u309A
+    # Unicode : \u309A: normal system
     def test_make_voicedsound_1_3(self):
         replaceCharacter = ReplaceCharacter()
-        assert replaceCharacter.make_voicedsound('ぱぴぷぺぽ') == 'ぱぴぷぺぽ'
+        assert replaceCharacter.make_voicedsound("ぱぴぷぺぽ") == "ぱぴぷぺぽ"
 
-    # Unicode : \u3099
+    # Unicode : \u3099: normal system
     def test_make_voicedsound_1_4(self):
         replaceCharacter = ReplaceCharacter()
-        assert replaceCharacter.make_voicedsound('ガギグゲゴザジズゼゾ') == 'ガギグゲゴザジズゼゾ'
+        assert replaceCharacter.make_voicedsound("ガギグゲゴザジズゼゾ") == "ガギグゲゴザジズゼゾ"
 
     def test_make_voicedsound_1_5(self):
         replaceCharacter = ReplaceCharacter()
-        assert replaceCharacter.make_voicedsound('ダヂヅデドバビブベボ') == 'ダヂヅデドバビブベボ'
+        assert replaceCharacter.make_voicedsound("ダヂヅデドバビブベボ") == "ダヂヅデドバビブベボ"
 
-    # Unicode : \u309A
+    # Unicode : \u309A: normal system
     def test_make_voicedsound_1_6(self):
         replaceCharacter = ReplaceCharacter()
-        assert replaceCharacter.make_voicedsound('パピプペポ') == 'パピプペポ'
+        assert replaceCharacter.make_voicedsound("パピプペポ") == "パピプペポ"
 
-    # dakuon
+    # dakuon: normal system
     def test_make_voicedsound_2_1(self):
         replaceCharacter = ReplaceCharacter()
-        assert replaceCharacter.make_voicedsound('がぎぐげござじずぜぞ') == 'がぎぐげござじずぜぞ'
+        assert replaceCharacter.make_voicedsound("がぎぐげござじずぜぞ") == "がぎぐげござじずぜぞ"
 
     def test_make_voicedsound_2_2(self):
         replaceCharacter = ReplaceCharacter()
-        assert replaceCharacter.make_voicedsound('だぢづてどばびぶべぼ') == 'だぢづてどばびぶべぼ'
+        assert replaceCharacter.make_voicedsound("だぢづでどばびぶべぼ") == "だぢづでどばびぶべぼ"
 
-    #handakuon
+    #handakuon: normal system
     def test_make_voicedsound_2_3(self):
         replaceCharacter = ReplaceCharacter()
-        assert replaceCharacter.make_voicedsound('ぱぴぷぺぽ') == 'ぱぴぷぺぽ'
+        assert replaceCharacter.make_voicedsound("ぱぴぷぺぽ") == "ぱぴぷぺぽ"
 
-    # dakuon
+    # dakuon: normal system
     def test_make_voicedsound_2_4(self):
         replaceCharacter = ReplaceCharacter()
-        assert replaceCharacter.make_voicedsound('ガギグゲゴザジズゼゾ') == 'ガギグゲゴザジズゼゾ'
+        assert replaceCharacter.make_voicedsound("ガギグゲゴザジズゼゾ") == "ガギグゲゴザジズゼゾ"
 
     def test_make_voicedsound_2_5(self):
         replaceCharacter = ReplaceCharacter()
-        assert replaceCharacter.make_voicedsound('ダヂヅデドバビブベボ') == 'ダヂヅデドバビブベボ'
+        assert replaceCharacter.make_voicedsound("ダヂヅデドバビブベボ") == "ダヂヅデドバビブベボ"
 
-    # handakuon
+    # handakuon: normal system
     def test_make_voicedsound_2_6(self):
         replaceCharacter = ReplaceCharacter()
-        assert replaceCharacter.make_voicedsound('パピプペポ') == 'パピプペポ'
+        assert replaceCharacter.make_voicedsound("パピプペポ") != "パピプペポ"
+
+    # dakuon: semi-normal system
+    def test_make_voicedsound_3_1(self):
+        replaceCharacter = ReplaceCharacter()
+        assert replaceCharacter.make_voicedsound("がぎぐげござじずぜぞ") != "かきくけこさしすせそ"
+
+    def test_make_voicedsound_3_2(self):
+        replaceCharacter = ReplaceCharacter()
+        assert replaceCharacter.make_voicedsound("だぢづでどばびぶべぼ") != "たちつてとはひふへほ"
+
+    #handakuon: semi-normal system
+    def test_make_voicedsound_3_3(self):
+        replaceCharacter = ReplaceCharacter()
+        assert replaceCharacter.make_voicedsound("ぱぴぷぺぽ") != "はひふへほ"
+
+    # dakuon: semi-normal system
+    def test_make_voicedsound_3_4(self):
+        replaceCharacter = ReplaceCharacter()
+        assert replaceCharacter.make_voicedsound("ガギグゲゴザジズゼゾ") != "カキクケコサシスセソ"
+
+    def test_make_voicedsound_3_5(self):
+        replaceCharacter = ReplaceCharacter()
+        assert replaceCharacter.make_voicedsound("ダヂヅデドバビブベボ") != "タチツテトハヒフヘホ"
+
+    # handakuon: semi-normal system
+    def test_make_voicedsound_3_6(self):
+        replaceCharacter = ReplaceCharacter()
+        assert replaceCharacter.make_voicedsound("パピプペポ") != "ハヒフヘホ"

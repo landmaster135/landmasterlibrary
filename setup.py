99e4f7b6a2d2b09d6d8e9e332ccc868e828192ef
landmaster135 <52403447+landmaster135@users.noreply.github.com>
2022-02-25 13:33:43 +0900

    u

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 setup(
     name="landmasterlibrary",
     version="0.0.1",
-    description="Convenient tools for me.3",
+    description="Convenient tools for me.4",
     author="kinkinnbeer135ml",
     author_email="52403447+landmaster135@users.noreply.github.com",
     license="MIT",

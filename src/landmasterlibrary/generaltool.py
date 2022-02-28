e792af3158b17b08c2edec0e671166fc079ccfa8
landmaster135 <52403447+landmaster135@users.noreply.github.com>
2022-03-01 00:51:52 +0900

    update

--- src/landmasterlibrary/generaltool.py
+++ src/landmasterlibrary/generaltool.py
@@ -116,6 +116,8 @@ def remove_head_sapces(word : str, spaces : list = Config.spaces) -> str:
     word_removed_space = ""
     print("===========================")
     print(word)
+    print(word[0])
+    print(spaces)
     if word[0] in spaces:
         word_removed_space = word[1:len(word)+1]
         # invisible head character if head space is nothing
